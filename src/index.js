70f2099b97c3866bf3f82cfbe95c43d4692e9a32
bilkn <65674342+bilkn@users.noreply.github.com>
2021-01-21 21:25:16 +0300

    feat: update webpack.config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "./style.scss";
+import "./test";
 alert('hello from webpack');
 alert('hello from webpack');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
@@ -8,14 +11,40 @@ module.exports = {
     filename: 'main.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: "./src/template.html"
-  })], 
+  optimization: {
+    minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'style.[contenthash].css' }),
+    new HtmlWebpackPlugin({
+      filename: 'index.[contenthash].html',
+      /* template: './src/template.html' */
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        removeComments: true,
+      },
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'imgs',
+          },
+        },
       },
     ],
   },
