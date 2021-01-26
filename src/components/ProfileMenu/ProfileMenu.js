29522060fc9563a1061b81ae0e8b06c79bf8aea2
bilkn <65674342+bilkn@users.noreply.github.com>
2021-01-26 21:21:12 +0300

    feat: add ProfileMenu

--- src/components/ProfileMenu/ProfileMenu.js
+++ src/components/ProfileMenu/ProfileMenu.js
@@ -1,11 +1,36 @@
-import React from 'react'
-import "./ProfileMenu.scss";
+import React from 'react';
+import './ProfileMenu.scss';
 function ProfileMenu() {
-    return (
-        <div className="profile-menu">
-            
-        </div>
-    )
+  return (
+    <div className="profile-menu">
+      <ul className="profile-menu-list">
+        <li className="profile-menu-list__item">
+          <i className="fas fa-cog profile-menu-list__icon" />
+          <a href="#" className="profile-menu-list__link">
+            Preferences
+          </a>
+        </li>
+        <li className="profile-menu-list__item">
+          <i className="fas fa-user-edit profile-menu-list__icon" />
+          <a href="#" className="profile-menu-list__link">
+            Edit Profile
+          </a>
+        </li>
+        <li className="profile-menu-list__item">
+          <i className="fas fa-question-circle profile-menu-list__icon" />
+          <a href="#" className="profile-menu-list__link">
+            Help
+          </a>
+        </li>
+        <li className="profile-menu-list__item no-border">
+          <i className="fas fa-envelope profile-menu-list__icon" />
+          <a href="#" className="profile-menu-list__link">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default ProfileMenu
+export default ProfileMenu;
