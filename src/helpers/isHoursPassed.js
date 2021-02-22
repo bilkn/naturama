336d6ba2c50b2c9b9c47f4184cf0153bf62c0ce0
bilkn <65674342+bilkn@users.noreply.github.com>
2021-02-22 13:15:12 +0300

    feat: add isHoursPassed helper

--- src/helpers/isHoursPassed.js
+++ src/helpers/isHoursPassed.js
@@ -1,3 +1,8 @@
-function isHoursPassed (hours, date) {
-    
+function isHoursPassed(hours, oldDate) {
+  const today = new Date();
+  const todayInMS = today.getTime();
+  const oldDateInMS = oldDate.getTime();
+  const result = todayInMS - oldDateInMS;
+  return Math.round(result / 1000 / 60 / 60) >= hours;
 }
+export default isHoursPassed;
