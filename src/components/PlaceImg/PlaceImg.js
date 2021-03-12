dcb14e4a9bb53d6f77d3aa3f522df669d0e0a66d
bilkn <bilkan.konus@hotmail.com>
2021-03-12 16:39:52 +0300

    feat: add srcset attribute to PlaceImg

--- src/components/PlaceImg/PlaceImg.js
+++ src/components/PlaceImg/PlaceImg.js
@@ -2,10 +2,20 @@ import React from 'react';
 import './PlaceImg.scss';
 
 function PlaceImg({ place }) {
-  const img = (place.img && place.img.large.source) || place.preview;
-  const width = (place.img && place.img.large.width) || 300;
+  const largeImg = place.img?.large?.source || place.preview;
+  const mediumImg = place.img?.medium?.source || place.preview;
+  const smallImg = place.img?.small?.source || place.preview;
+  const width = place.img?.large?.width || 300;
+  // !!! Could be refactored in the future.
   const { name } = place.content;
-  return <img src={img} alt={name} className="place-img" width={width} />;
+  return (
+    <img
+      srcSet={`${smallImg} 250w,${mediumImg} 500w, ${largeImg} 1000w`}
+      alt={name}
+      className="place-img"
+      width={width}
+    />
+  );
 }
 
 export default PlaceImg;
