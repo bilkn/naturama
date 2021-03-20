9b56b1b4f0b143ac263ab12c105d418d25d22a7c
bilkn <bilkan.konus@hotmail.com>
2021-03-20 13:07:19 +0300

    feat: add lazy load

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,17 +9,25 @@ import UserProvider from '../../providers/UserProvider/UserProvider';
 import ErrorProvider from '../../providers/ErrorProvider/ErrorProvider';
 import UserRequestProvider from '../../providers/UserRequestProvider/UserRequestProvider';
 import Home from '../../pages/Home/Home';
-import Favourites from '../../pages/Favourites/Favourites';
-import DailyPlaceList from '../../pages/DailyPlaceList/DailyPlaceList';
-import FullscreenPicture from '../../pages/FullscreenPicture/FullScreenPicture';
-import Preferences from '../Preferences/Preferences';
-import Profile from '../../pages/Profile/Profile';
 import MobileNav from '../MobileNav/MobileNav';
-import Help from '../../pages/Help/Help';
-import Map from '../../pages/Map/Map';
 import NotificationContainer from '../NotificationContainer/NotificationContainer';
 import AppHead from '../AppHead/AppHead';
 import LoadingProvider from '../../providers/LoadingProvider/LoadingProvider';
+import Loader from '../Loader/Loader';
+
+const DailyPlaceList = React.lazy(() =>
+  import('../../pages/DailyPlaceList/DailyPlaceList')
+);
+const Favourites = React.lazy(() =>
+  import('../../pages/Favourites/Favourites')
+);
+const Profile = React.lazy(() => import('../../pages/Profile/Profile'));
+const FullscreenPicture = React.lazy(() =>
+  import('../../pages/FullscreenPicture/FullScreenPicture')
+);
+const Map = React.lazy(() => import('../../pages/Map/Map'));
+const Preferences = React.lazy(() => import('../Preferences/Preferences'));
+const Help = React.lazy(() => import('../../pages/Help/Help'));
 
 function App() {
   return (
@@ -37,19 +45,21 @@ function App() {
                       <NotificationContainer />
                       <Switch>
                         <Route exact path="/" component={Home} />
-                        <Route path="/favourites" component={Favourites} />
-                        <Route path="/profile" component={Profile} />
-                        <Route
-                          path="/daily-place-list"
-                          component={DailyPlaceList}
-                        />
-                        <Route
-                          path="/fullscreen-picture"
-                          component={FullscreenPicture}
-                        />
-                        <Route path="/preferences" component={Preferences} />
-                        <Route path="/help" component={Help} />
-                        <Route path="/map" component={Map} />
+                        <React.Suspense fallback={<Loader />}>
+                          <Route path="/favourites" component={Favourites} />
+                          <Route path="/profile" component={Profile} />
+                          <Route
+                            path="/daily-place-list"
+                            component={DailyPlaceList}
+                          />
+                          <Route
+                            path="/fullscreen-picture"
+                            component={FullscreenPicture}
+                          />
+                          <Route path="/preferences" component={Preferences} />
+                          <Route path="/help" component={Help} />
+                          <Route path="/map" component={Map} />
+                        </React.Suspense>
                       </Switch>
                     </Router>
                   </LoadingProvider>
